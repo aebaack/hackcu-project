7d0b751045ced8e1874066c8d9243d2b8df6380f
mark-wilmes <mawi0949@colorado.edu>
2018-12-01 10:46:09 -0700

    Express app created

--- api/server.js
+++ api/server.js
@@ -3,11 +3,14 @@
 
 const SteamAPI = require('steamapi');
 const steam = new SteamAPI('23C3817E9A5B7DC2622F180C62A0946A');
+var express = require('express');
+var app = express();
 
 function getUserID(steamid) {
 
   steam.resolve(`https://steamcommunity.com/id/${steamid}`).then(id => {
     console.log(id);
+    return id
   });
 
 }
@@ -16,4 +19,15 @@ function getUserInfo(userid) {
 
 }
 
-getUserID('trailerparkjesus');
+app.get('/',function(req,res){
+  var id = getUserID('trailerparkjesus');
+  res.send(id);
+})
+//getUserID('trailerparkjesus');
+
+var server = app.listen(8080, function () {
+  var host = server.address().address
+  var port = server.address().port
+  
+  console.log("Example app listening at http://%s:%s", host, port)
+})
