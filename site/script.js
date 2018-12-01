b6f1b5e789a2ed1a90868cabc61f9623e41b1363
Willem Mirkovich <wjmirk@rgnt1-62-14-dhcp.int.colorado.edu>
2018-12-01 13:53:45 -0700

    fixed button issue

--- site/script.js
+++ site/script.js
@@ -1,6 +1,8 @@
 $(document).ready(() => {
     document.getElementById("searchButton").addEventListener("click", function(){
         console.log("click works");
+        var input = document.getElementById("searchSteamID").value;
+        console.log(input);
         $.get("http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=440&format=xml", function(data){
             console.log("succees");
             console.log(data);
