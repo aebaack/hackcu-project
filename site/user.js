8fc7f8b051be7aca99559ae84a3fe27c54e9b85f
Aidan Baack <aebaack@gmail.com>
2018-12-01 14:59:18 -0700

    Insert table

--- site/user.js
+++ site/user.js
@@ -6,4 +6,17 @@ $(document).ready(() => {
   $('#steamID').text(userObj.user.steamID);
   $('#accountDate').text(userObj.user.created);
 
+  userObj.games.sort((g1, g2) => g2.playTime - g1.playTime);
+  userObj.games.forEach(g => {
+    if (g.playTime < 60) {
+      time = `${g.playTime} minutes`
+    } else {
+      time = `${Math.floor(g.playTime / 60)}H ${g.playTime % 60}M`
+    }
+
+    playtime = toString(g.playTime / 60);
+    playtime.indexOf('.');
+
+    $('#game-list').append(`<tr><td>${g.name}<br>${time}</td></tr>`)
+  });
 });
