53a5af9cd0ea7913c115846643d23ff88a16056b
Adam Bender <adambender@rgnt1-14-205-dhcp.int.colorado.edu>
2018-12-01 13:26:02 -0700

    User Info Screen Update

--- site/user.js
+++ site/user.js
@@ -3,4 +3,6 @@ userObj = JSON.parse(localStorage.getItem('userObj'));
 $(document).ready(() => {
   $('#user-name').text(userObj.user.nickname);
   $('#avatar').attr('src', userObj.user.avatar.large);
+  $('#steamID').text(userObj.user.steamID);
+  $('#accountDate').text(userObj.user.created)
 });
