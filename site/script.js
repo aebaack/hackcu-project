a8c06ec4a119b154a179a479086e126faead8d98
Willem Mirkovich <wjmirk@rgnt1-62-14-dhcp.int.colorado.edu>
2018-12-01 14:02:08 -0700

    adding index working

--- site/script.js
+++ site/script.js
@@ -3,9 +3,10 @@ $(document).ready(() => {
         console.log("click works");
         var input = document.getElementById("searchSteamID").value;
         console.log(input);
-        $.get("http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=440&format=xml", function(data){
+        $.get("https://hackcu-api.herokuapp.com/user/" + input + "", function(data){
             console.log("succees");
             console.log(data);
+            localStorage.setItem('userObj', JSON.stringify(data));
         });
     });
 });
