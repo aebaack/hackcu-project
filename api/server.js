24df0ac3bd971b7cf48d729b4ec4b8c90e61e80d
Aidan Baack <aebaack@gmail.com>
2018-12-01 12:09:01 -0700

    Create user info get

--- api/server.js
+++ api/server.js
@@ -3,31 +3,32 @@
 
 const SteamAPI = require('steamapi');
 const steam = new SteamAPI('23C3817E9A5B7DC2622F180C62A0946A');
-var express = require('express');
-var app = express();
 
-function getUserID(steamid) {
+const express = require('express');
+const app = express();
 
-  steam.resolve(`https://steamcommunity.com/id/${steamid}`).then(id => {
-    console.log(id);
-    return id
-  });
+app.get('/user/:user', (req, res) => {
 
-}
+  steam.resolve(`https://steamcommunity.com/id/${req.params.user}`)
+    .then(userid => {
 
-function getUserInfo(userid) {
+      const userInfo = Promise.all([steam.getUserSummary(userid),
+        steam.getUserOwnedGames(userid),
+        steam.getUserFriends(userid)]);
 
-}
-
-app.get('/',function(req,res){
-  var id = getUserID('trailerparkjesus');
-  res.send(id);
+      // Return data
+      userInfo.then(user => {
+        res.send({
+          'user': user[0],
+          'games': user[1],
+          'friends': user[2]
+        });
+      })
+    })
 })
-//getUserID('trailerparkjesus');
-
-var server = app.listen(8080, function () {
-  var host = server.address().address
-  var port = server.address().port
   
+const server = app.listen(8080, function () {
+  const host = server.address().address
+  const port = server.address().port
   console.log("Example app listening at http://%s:%s", host, port)
 })
